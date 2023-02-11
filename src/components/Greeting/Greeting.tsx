aaab2049be71ab9b9ac2823a1b79b66a52e9bf9c
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2023-02-12 01:36:20 +0530

    fix: greeting componemt data fetching

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,16 +1,16 @@
 import { Button } from '@components/Button';
 import { Card } from '@components/Card';
 import { Logout } from './Logout';
+import { getUserDetails } from '@lib/server_side_data_fetching/user_data';
 
 export const Greeting = async () => {
-  // const user = await getUserDetails();
+  const user = await getUserDetails();
 
   return (
     <Card className='w-full py-4 flex flex justify-between'>
       <div className='mb-2'>
         <h1 className='text-3xl text-gray-700 font-bold mb-4'>
-          {/* Hello, {user?.firstName}! */}
-          hello
+          Hello, {user?.firstName}! hello
         </h1>
         <h4 className='text-xl text-gray-400'>
           Check your daily tasks and schedule
